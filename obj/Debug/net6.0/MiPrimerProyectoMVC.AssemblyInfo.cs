3b3aacdb4be8976c50db89cbdc21455c47f4b9ba
EliezerGlez <eliezerglez@gmail.com>
2024-12-21 22:14:11 +0000

    Actualizar a .NET6 SDK

--- obj/Debug/net6.0/MiPrimerProyectoMVC.AssemblyInfo.cs
+++ obj/Debug/net6.0/MiPrimerProyectoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiPrimerProyectoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c88359bf7d398d378fe34e000f6d9e78bd13874e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f2c1d2caa43b201ad1285b82086620c52571c69")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiPrimerProyectoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiPrimerProyectoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
